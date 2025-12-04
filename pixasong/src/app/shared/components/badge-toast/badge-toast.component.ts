9b2173c525d71466853fee1d884d0a8bc6b8c445
medabdr <meds89601@gmail.com>
2025-12-04 20:38:26 +0000

    add structur

---
+++ pixasong/src/app/shared/components/badge-toast/badge-toast.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-badge-toast',
+  standalone: true,
+  imports: [],
+  templateUrl: './badge-toast.component.html',
+  styleUrl: './badge-toast.component.css'
+})
+export class BadgeToastComponent {
+
+}
